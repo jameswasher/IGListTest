993b6ef89ab075ee99534fe89dd578ba4b15e410
James Washer <james.washer@gmail.com>
2016-12-18 12:51:50 -0600

    CardSectionController uses LessonSectionViewModel instead of Lesson.  This contains an immutable array of cards.  Safer.

---
+++ IGListTesting/LessonSectionViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  LessonSectionViewModel.swift
+//  IGListTesting
+//
+//  Created by James Washer on 12/18/16.
+//  Copyright © 2016 James Washer. All rights reserved.
+//
+
+import UIKit
+
+class LessonSectionViewModel: NSObject {
+
+}
